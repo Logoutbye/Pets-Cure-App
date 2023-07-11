b6b02e277154b6227c6a01f58d14306952844606
Bilal Muhammad <91424005+Logoutbye@users.noreply.github.com>
2023-07-10 22:11:07 -0700

    Create my_colors.dart

---
+++ my_colors.dart
@@ -0,0 +1,8 @@
+// i will delete it later
+
+import 'package:flutter/material.dart';
+
+class MyColors{
+  static const  kprimaryColor = Color(0xFF04abf2);
+  static const  ksecondaryColor = Color.fromARGB(255, 0, 0, 0);
+}
