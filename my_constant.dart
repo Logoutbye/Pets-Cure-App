5d38a4ac1f718132961b16b123c55eea885778e1
Bilal Muhammad <91424005+Logoutbye@users.noreply.github.com>
2023-07-10 22:14:43 -0700

    Create my_constant.dart

---
+++ my_constant.dart
@@ -0,0 +1,10 @@
+//i wil delete it too 
+import 'package:flutter/material.dart';
+
+class MyColors{
+  static const  kprimaryColor = Color(0xFF04abf2);
+  static const  ksecondaryColor = Color.fromARGB(255, 0, 0, 0);
+  static const  ksecondaryColor = Color.fromARGB(255, 0, 0, 0);
+  static const  ksecondaryColor = Color.fromARGB(255, 0, 0, 0);
+  static const  ksecondaryColor = Color.fromARGB(255, 0, 0, 0);
+}
